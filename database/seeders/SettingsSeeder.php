ff6ab2b656d005cb628676740736f0422dfd182c
Abdul Hadi <abdulhadijatoi@gmail.com>
2025-10-17 15:09:47 +0500

    updated serttings seeder

--- database/seeders/SettingsSeeder.php
+++ database/seeders/SettingsSeeder.php
@@ -21,7 +21,22 @@ class SettingsSeeder extends Seeder
             ],
             [
                 'key' => 'contact_email',
-                'value' => 'info@chauffeurservices.com',
+                'value' => 'info@chauffeurdubai.com',
+                'is_file' => false,
+            ],
+            [
+                'key' => 'location',
+                'value' => 'Dubai, UAE',
+                'is_file' => false,
+            ],
+            [
+                'key' => 'address',
+                'value' => 'Business Bay, Dubai, United Arab Emirates',
+                'is_file' => false,
+            ],
+            [
+                'key' => 'contact_phone',
+                'value' => '+971 52 308 7786',
                 'is_file' => false,
             ],
             [
